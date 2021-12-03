125a523156fc33c831fd73355194b7491af8aa8f
cesarsnina <cesar.nina05@gmail.com>
2021-12-03 19:23:21 +0000

    added validation

--- server/model/User.js
+++ server/model/User.js
@@ -3,8 +3,21 @@ const { db, DataTypes, Model } = require('../db');
 class User extends Model {}
 
 User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
+    name: { 
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notEmpty: true
+        }
+    }
 },{
     sequelize,
     timestamps: false,
