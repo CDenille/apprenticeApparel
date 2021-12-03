7edaa936b36913618a8ab603e963fb2c447c37ea
CDenille <carringtondenille@gmail.com>
2021-12-03 18:15:24 +0000

    updated storage

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const {Sequelize, DataTypes, Model} = require('sequelize')
 
 const db = new Sequelize('database', 'username', 'password', {
     dialect: 'sqlite',
-    storage: './myTVShows.sqlite',
+    storage: './ApprenticeApparel.sqlite',
     logging: false
 })
 
