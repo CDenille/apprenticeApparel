b0bb01609d88ec78de0b5f5441d7a2b5fbe34e9b
CDenille <carringtondenille@gmail.com>
2021-12-03 19:11:51 +0000

    Item model added

---
+++ server/model/Item.js
@@ -0,0 +1,16 @@
+const {db, DataTypes, Model} = require('../db')
+
+class Item extends Model {
+    
+}
+
+Item.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.DOUBLE, 
+    image: DataTypes.STRING
+}, {
+    sequelize: db
+})
+
+module.exports = {Item}
