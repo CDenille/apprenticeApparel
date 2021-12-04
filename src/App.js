1c6007ee38b9d81d5b046c2a0a7e0f5d35c30aaa
cesarsnina <cesar.nina05@gmail.com>
2021-12-04 15:04:00 +0000

    install react-router-dom and created an example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import "./App.css";
+import { Routes, Route, Link } from 'react-router-dom';
+
+
+const Red = () => {
+    return (
+        <h1>RED!</h1>
+    )
+}
+
+const Blue = () => {
+    return (
+        <h1>BLUE!</h1>
+    )
+}
 
 const App = () => {
     return (
         <div>
-            <h1>Hi there!!!!</h1>
-            <p>How are you??</p>
-            <p>This is a test!!</p>
-            <p>Denille is here</p>
+            <div>
+                <div>
+                    <h1>Hi there!!!!</h1>
+                    <p>How are you??</p>
+                    <p>This is a test!!</p>
+                    <p>Denille is here</p>
+                    <p><Link to='/red'>Go to Red!</Link></p>
+                    <p><Link to='/blue'>Go to Blue!</Link></p>
+                </div>
+
+                <div>
+                    <Routes>
+                        <Route path='/red' element={<Red />} />
+                        <Route path='/blue' element={<Blue />} />
+                    </Routes>
+                </div>
+            </div>
         </div>
     )
 }
