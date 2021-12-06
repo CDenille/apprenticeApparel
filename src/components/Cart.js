a4c63f48b046252ed7e3411a27f8d093c902340c
cesarsnina <cesar.nina05@gmail.com>
2021-12-06 02:10:57 +0000

    created a few files for our pages

---
+++ src/components/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Cart.css";
+
+const Cart = () => {
+    return (
+        <div>
+            <h1>This is the Cart Component!!</h1>
+        </div>
+    )
+}
+
+export default Cart;
