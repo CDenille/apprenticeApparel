2b6a42565b8aa9bb43dbaf0e853e5c317c3b848e
cesarsnina <cesar.nina05@gmail.com>
2021-12-07 17:53:42 +0000

    completed the visual of the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col, Form, FloatingLabel } from 'react-bootstrap';
 import Logo from '../../Logo.png'
 import './Contact.css';
 
@@ -64,7 +64,42 @@ const Contact = () => {
                 </Row>
             </Col>
             <Col>
-                <h3 className="contact-header">Contact us here</h3>
+                <Row>
+                    <Col><h3 className="contact-header">Contact us here</h3></Col>
+                </Row>
+                <Row>
+                <Col>
+                <Form>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Full name"
+                        className="mb-3">
+                        <Form.Control type="name" placeholder="James Bond" />
+                    </FloatingLabel>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Email address"
+                        className="mb-3">
+                        <Form.Control type="email" placeholder="name@example.com" />
+                    </FloatingLabel>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="Phone number"
+                        className="mb-3">
+                        <Form.Control type="phone number" placeholder="555-555-5555" />
+                    </FloatingLabel>
+                    <FloatingLabel controlId="floatingTextarea2" label="Message" className="mb-3">
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: '100px' }}/>
+                    </FloatingLabel>
+                    <Button className="button" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+                </Col>
+                </Row>
             </Col>
             </Row>
         </Container>
