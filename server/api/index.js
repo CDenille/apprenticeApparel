b04da8cd1be865adfdd874951ed4226bd16c65e1
cesarsnina <cesar.nina05@gmail.com>
2021-12-09 23:15:16 +0000

    wrote some notes for the routes

---
+++ server/api/index.js
@@ -0,0 +1,43 @@
+const { post } = require("request")
+
+const { put } = require("request")
+
+
+/aa/users
+/aa/users/:id 
+/aa/users/:id/cart
+/aa/items/:id
+/aa/admin 
+/aa/cart
+
+
+/aa - home - Category
+/aa/women /men /jewelry /electronics - Item
+/aa/signup - post() - User
+/aa/login - User 
+/aa/adminView - item 
+/aa/adminView/:id - put() - Item
+/aa/contactus/ -post() - console.log - User?
+/aa/checkout/:id - Cart 
+/aa/sale - item 
+/aa/users - users
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
